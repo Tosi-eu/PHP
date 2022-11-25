20a8eb4db5dd98a7d32c432a173660bba79b312a
Tosiapenas <galdiniguilherme@usp.br>
2022-11-25 14:33:41 -0300

    added migrations (started model gen)

--- backend/config/main.php
+++ backend/config/main.php
@@ -41,15 +41,13 @@ return [
             'bundles' => [
                 \yii\bootstrap5\BootstrapAsset::class => false
             ]
-        ]
-        /*
+        ],
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
